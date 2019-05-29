f669945bf45befa7bf9682529d6595fc7c95e38b
maryamkarandish <37395709+maryamkarandish@users.noreply.github.com>
2019-05-29 11:10:04 +0430

    Update WebSecurityConfiguration.java

--- explorecali/src/main/java/com/example/ec/security/WebSecurityConfiguration.java
+++ explorecali/src/main/java/com/example/ec/security/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,6 +39,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+    
+     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("letmein")).roles("ADMIN").and()
+                .withUser("user").password(passwordEncoder().encode("letmein")).roles("USER");
     }
 
     @Bean
